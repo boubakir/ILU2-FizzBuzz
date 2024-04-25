7063f2fb2d59146068a1d2f60d87b030783112b8
boubakir <boubakirmohamed@yahoo.fr>
2024-04-25 12:50:17 +0200

    couverture

--- src/fizzBuzz/FizzBuzz.java
+++ src/fizzBuzz/FizzBuzz.java
@@ -2,4 +2,35 @@ package fizzBuzz;
 
 public class FizzBuzz {
 
+
+	/*public static Object fizzBuzz(int i) {
+		String sres = "";
+		if (i%3 == 0) sres += "fizz";
+		if (i%5 == 0) sres += "buzz";
+		if(sres.equals(""))
+			return Integer.toString(i);
+		else
+			return sres; 
+	}*/
+	
+	
+	//Refactor
+	
+	static Integer[] valSpec = {3, 5};
+	static String[] motsSpec = {"fizz", "buzz"};
+	
+	public static String fizzBuzz(int entree) {
+		StringBuilder result = new StringBuilder("");
+		for(int i = 0; i < valSpec.length; i++  ) {
+			if (entree%valSpec[i] == 0) result.append(motsSpec[i]);
+		}
+		if(result.length()==0)
+			return Integer.toString(entree);
+		else
+			return result.toString(); 
+	}
+	
+	
+	
+	
 }
--- test/fizzBuzz/FizzBuzzTest.java
+++ test/fizzBuzz/FizzBuzzTest.java
@@ -1,14 +1,55 @@
 package fizzBuzz;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
+	
+	
+	
+	@Test
+	void test_un() {
+		assertEquals(FizzBuzz.fizzBuzz(1), "1");
+	}
+	
+	@Test
+	void test_deux() {
+		assertEquals(FizzBuzz.fizzBuzz(2), "2");
+	}
+	
+	@Test
+	void test_trois() {
+		assertEquals(FizzBuzz.fizzBuzz(3), "fizz");
+	}
+	
+	@Test
+	void test_quelques_Valeurs() {
+		assertEquals(FizzBuzz.fizzBuzz(4), "4");
+		assertEquals(FizzBuzz.fizzBuzz(7), "7");
+	}
+	
+	@Test
+	void test_cinq() {
+		assertEquals(FizzBuzz.fizzBuzz(5), "buzz");
+	}
+	
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_multiples() {
+		assertEquals(FizzBuzz.fizzBuzz(6), "fizz");
+		assertEquals(FizzBuzz.fizzBuzz(9), "fizz");
+		assertEquals(FizzBuzz.fizzBuzz(10), "buzz");
+		assertEquals(FizzBuzz.fizzBuzz(400), "buzz");
 	}
+	
+	@Test
+	void test_multiples_double() {
+		assertEquals(FizzBuzz.fizzBuzz(3*5), "fizzbuzz");
+		assertEquals(FizzBuzz.fizzBuzz(2*3*5), "fizzbuzz");
+		assertEquals(FizzBuzz.fizzBuzz(3*3*5*5), "fizzbuzz");
+	}
+	
 
 }
